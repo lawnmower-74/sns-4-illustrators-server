a0a9f4667cc43eb8d5acd2a45cedff89b91b1a4e
lawnmower-74 <tonapricot@gmail.com>
2026-01-21 01:02:21 +0900

    ファイル名でも検索する予定のためインデックスを追加

--- models/image.go
+++ models/image.go
@@ -5,7 +5,7 @@ import "time"
 type Image struct {
 	ID              uint       `gorm:"primaryKey"`
 	IllustratorName string     `gorm:"column:illustrator_name;size:255;not null;index"`
-	FileName        string     `gorm:"column:file_name;size:255;not null;unique"`
+	FileName        string     `gorm:"column:file_name;size:255;not null;unique;index"`
 	FileSize        int64      `gorm:"column:file_size;not null"`
 	MimeType        string     `gorm:"column:mime_type;size:50;not null"`
 	StoragePath     string     `gorm:"column:storage_path;size:500;not null"`
